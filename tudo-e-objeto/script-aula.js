4b21d63026944dacb94bc64ff792001b1e4d4a8b
Paulo Vitor <paulovitorvaztst@gmail.com>
2023-03-08 15:35:09 -0300

    aula + exercicios sobre objeto e suas propriedades

---
+++ tudo-e-objeto/script-aula.js
@@ -0,0 +1,5 @@
+var nome = 'André'
+
+var nome_pequeno = nome.toLowerCase()
+
+var btn = document.querySelector('.btn')
---
+++ tudo-e-objeto/script.js
@@ -0,0 +1,16 @@
+//nomeie 3 propriedades ou métodos de strings
+var nome = 'André'
+//fixed
+//length
+//slice
+
+//nomeie 5 propiedades ou métodos de elemento do DOM
+var btn = document.querySelector('.btn')
+// addEventListener
+//appendChild
+//id
+//innerHTML
+console.log(btn)
+
+//busque no web um objeto (método) capaz de interagir com clipboard,
+//clipboard é a porta do eu computador que lida com o CTRL + C e CTRl + V
