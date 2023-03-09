0f946c299c73720f04899f4aca1e6d26ffcf8760
Paulo Vitor <paulovitorvaztst@gmail.com>
2023-03-09 19:54:27 -0300

    aula-exercicios atribuição e ternario

---
+++ atribuicao-e-ternario/script-aula.js
@@ -0,0 +1,12 @@
+var idade = 20
+var possuiDiabetes = true
+
+var podeBeber = (idade >= 18 && possuiDiabetes) ? 'Pode beber' : 'Não pode beber'
+
+console.log(podeBeber)
+
+var possuiFaculdade = false
+if(possuiFaculdade)
+    console.log('sim possui')
+else
+    console.log('Não possui')
---
+++ atribuicao-e-ternario/script.js
@@ -0,0 +1,12 @@
+// Some 500 se valor de scroll abaixo, atribuindo a novo valor a scroll
+
+var scroll = 1000
+scroll += 500
+console.log(scroll)
+
+//atribua true para a variável darCredito, caso o cliente possua carro e casa. e false caso o contrario
+var possuiCarro = true
+var possuiCasa = true
+var darCredito;
+darCredito = (possuiCarro && possuiCasa) 
+console.log(darCredito)
