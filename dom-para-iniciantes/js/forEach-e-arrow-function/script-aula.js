313bc6313be4b9e64e707ac0ce391ffe39217a8f
Paulo Vitor <paulovitorvaztst@gmail.com>
2023-03-13 17:17:18 -0300

    aula-atividade sobre forEach e arrow function

---
+++ dom-para-iniciantes/js/forEach-e-arrow-function/script-aula.js
@@ -0,0 +1,16 @@
+// const imgs = document.querySelectorAll('img')
+
+// imgs.forEach(function(item, index,array){
+//     // console.log(item,index,array)
+// })
+
+const titulos = document.getElementsByClassName('titulo')
+const titulosArray = Array.from(titulos)
+
+// console.log(titulos)
+// console.log(titulosArray)
+
+titulosArray.forEach((item) => {
+    console.log(item)
+})
+
