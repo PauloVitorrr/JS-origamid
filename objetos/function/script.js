98519dcd56c88a5ef1016ff2ced8dd249075015b
Paulo Vitor <paulovitorvaztst@gmail.com>
2023-06-17 22:59:44 -0300

    Aula e atividade sobre funções

--- objetos/function/script.js
+++ objetos/function/script.js
@@ -1,67 +1,33 @@
-// const perimetro = new Function('lado', 'return lado * 4')
+// Retorne a soma total de caracteres dos
+// parágrafos acima utilizando reduce
+const paragrafos = document.querySelectorAll('p')
 
-// function somar(n1, n2, n4){
-//     return n1 + n2  
-// }
-
-// console.log(somar.name)
-
-// function darOi(){
-//     console.log('Oi para voce')
-// }
-
-// darOi.call(null, 'André', 28)
-
-// window.marca = 'Carro'
-// window.ano = 288
-
-// function descricaoCarro(velocidade){
-//     console.log(this)
-//     console.log(this.marca + ' ' + this.ano + velocidade)
-// }
-
-// descricaoCarro.call({marca:'Honda', ano: 2015}, 100)
-
-const carros = ['Ford', 'Fiat', 'VW']
-const frutas = ['Banana', 'Uva', 'Pêra']
-
-carros.forEach.call(frutas, (item) =>{
-    console.log(item)
+const totalCaracteres = Array.prototype.reduce.call(paragrafos, (acumulador, item) =>{
+    return acumulador + item.innerText.length;
 })
 
-function Dom(seletor){
-    this.element = document.querySelector(seletor);
+console.log(totalCaracteres)
+console.log(paragrafos)
+// Crie uma função que retorne novos elementos
+// html, com os seguintes parâmetros
+// tag, classe e conteudo.
+function criarElemento(tag, classe, conteudo){
+    const elemento = document.createElement(tag)
+    classe ? elemento.classList.add(classe) : null
+    classe ? elemento.classList.innerHTML = conteudo : null
+    return elemento;
 }
 
-Dom.prototype.ativo = function(classe){
-    this.element.classList.add(classe)
-}
+console.log(criarElemento('li', 'azul', 'Esse é o conteudo'))
 
-const ul = new Dom('ul')
+// Crie uma nova função utilizando a anterior como base
+// essa nova função deverá sempre criar h1 com a
+// classe titulo. Porém o parâmetro conteudo continuará dinâmico
 
-// const li = {
-//     element: document.querySelector('li')
-// }
+const h1Titulo = criarElemento.bind(null, 'h1', 'titulo')
 
+const cursosJS = h1Titulo('Curso de javascript')
+const cursoHTML = h1Titulo('Curso de HTML')
 
-
-// ul.ativo.call(li, 'ativo')
-
-const arrayLike = {
-    0: 'Item 1',
-    1: 'Item 2',
-    2: 'Item 3',
-    3: 'Item 4',
-    length: 3,
-}
-
-const li = document.querySelectorAlll('li')
-const arrayLi = Array.from(li)
-
-const filtro = Array.prototype.filter.call(li, (item)=>{
-    return item.classList.contains('Ativo')
-})
-
-console.log(filtro)
-
-console.log(li)
+console.log(cursosJS, cursoHTML)
+console.log(h1Titulo('Curso de HTML'))
